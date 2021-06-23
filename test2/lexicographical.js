203cbc7243f7db54cc8c1ece1308393fb0d601ca
hilarion18 <hilarion_cross@hotmail.com>
2021-06-23 23:23:33 +0700

    updated done test2

--- test2/lexicographical.js
+++ test2/lexicographical.js
@@ -1,5 +1,9 @@
 var fs = require('fs')
 
 let data = fs.readFileSync('data.txt', 'utf8');
-let newData = data.toString().split("").filter((item, pos, self) => self.indexOf(item) == pos).join('')
-console.log("== new Data", newData);
+let newData = data.toString().split("").filter((item, pos, self) => self.indexOf(item) == pos).join("")
+
+let smallestLex = newData.split("").sort().join("");
+let biggestLex = smallestLex.split("").reverse().join("");
+console.log("The smallest lexicographical order is: " + smallestLex);
+console.log("The first occurence from left to right is: " + biggestLex);
